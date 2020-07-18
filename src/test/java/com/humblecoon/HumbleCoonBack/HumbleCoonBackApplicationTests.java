31c2d387272595f623e37055944b209f34e67f4e
Sergey Kozyakov <koziakov@ya.ru>
2020-07-18 23:55:22 +0300

    Spring Framework was added, Gradle was updated

---
+++ src/test/java/com/humblecoon/HumbleCoonBack/HumbleCoonBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humblecoon.HumbleCoonBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumbleCoonBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
